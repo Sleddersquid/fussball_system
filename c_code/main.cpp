d6b6b03d82ea24c7c640e7ed605177c63d9acd1b
ErikSNyborg <esnv96@gmail.com>
2024-10-09 19:00:26 +0100

    ys

---
+++ c_code/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <array>
+#include <vector>
+
+#include <gpiod.hpp>
+// #include <opencv4/opencv2/opencv.hpp>
+
+#include "library/main.hpp"
+
+int main() {
+    std::cout << "End of file" << std::endl;
+    return 0;
+}
